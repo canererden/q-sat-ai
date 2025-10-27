cfe342a237b032e73f8300cf97c2180ea5f7c469
Caner Erden <cerden@subu.edu.tr>
2025-10-27 10:10:36 +0300

    Initial Railway-ready Dockerized Flask app

--- app.py
+++ app.py
@@ -160,4 +160,6 @@ def about():
 
 # ========== 4️⃣ FLASK SUNUCUSU ==========
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
